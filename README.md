20be98b9c08fe680e3b06caafe591a874c5d8228
jhembe <jhembe202@gmail.com>
2023-04-04 12:33:11 +0300

    ADDED : Project from Local. Working Model

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# website-downloader
-A simple Python script that allows you to download a website and store it locally for offline access. It includes a progress bar, resume features and a limit for download size depending on the user's input.
+# Website Downloader
+
+A Python script to download a website and store it locally for offline access.
+
+## Requirements
+
+- Python 3.x
+- `wget` command line tool
+
+## Installation
+
+1. Clone or download this repository to your local machine.
+2. Install the required dependencies by running the following command in your terminal:
+
+``bash
+pip install -r requirements.txt
+
+## Usage
+
+To download a website, run the `download_website.py` script and provide the URL of the website you want to download, as well as the directory where you want to save the downloaded files:
+
+``bash
+python download_website.py
+
+The script will display a progress bar as the website is being downloaded. If the download is interrupted for any reason, you can resume it by running the same command again.
+
+By default, the script will download the entire website. If you want to limit the download size, you can provide a maximum size (in megabytes) using the following command:
+
+``bash
+python download_website.py --max-size <size>
+
+Replace `<size>` with the maximum size you want to allow for the download.
+
+## Example
+
+To download the OpenAI homepage and save it to the directory `/home/user/Downloads/OpenAI`, run the following command:
+
+``bash
+python download_website.py --url https://openai.com/ --output /home/user/Downloads/OpenAI
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Contributions are welcome! If you find a bug or have a feature request, please open an issue or submit a pull request.
+
+## Acknowledgements
+
+This project uses the following open source libraries:
+
+- [wget](https://pypi.org/project/wget/) - For downloading the website files.
+- [tqdm](https://pypi.org/project/tqdm/) - For displaying the progress bar.
+
+## Disclaimer
+
+Please be aware that downloading websites without permission may violate copyright laws in some countries. Use this script responsibly and only download websites that you have permission to access.
+
+## Contact
+
+If you have any questions or comments about this project, please feel free to contact me at [your-email@example.com](mailto:your-email@example.com).
---
+++ download_website.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+import sys
+
+from tqdm import tqdm
+
+def download_website(url, output_dir, max_size=None):
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Determine the output file to use for resuming the download
+    resume_file = os.path.join(output_dir, '.wget-resume')
+
+    # Set up the wget command with the appropriate options
+    command = ['wget', '--recursive', '--page-requisites', '--html-extension',
+               '--convert-links', '--no-parent', '-P', output_dir]
+
+    if max_size is not None:
+        command += ['--quota', max_size]
+
+    if os.path.exists(resume_file):
+        # If the resume file exists, try to resume the download
+        command += ['--continue', '--progress=dot:mega', '-o', '/dev/null',
+                    '-c', '-T', '10', '-t', '0', '-i', resume_file, url]
+    else:
+        # Otherwise, start a new download
+        command += ['--progress=dot:mega', '-o', '/dev/null', url]
+
+    try:
+        # Execute the wget command, displaying a progress bar
+        with subprocess.Popen(command, stdout=subprocess.PIPE,
+                              bufsize=1, universal_newlines=True) as process:
+            total_size = 0
+            for line in process.stdout:
+                # Parse the output of wget to update the progress bar
+                if line.startswith('Length:'):
+                    total_size = int(line.split()[1])
+                elif line.startswith('Saving to:'):
+                    file_name = line.split()[2]
+                elif line.startswith('Progress:'):
+                    progress = int(line.split()[1][:-1])
+                    if total_size > 0:
+                        pbar.update(progress - pbar.n)
+
+            # Write the resume file if the download was successful
+            if process.returncode == 0:
+                if os.path.exists(resume_file):
+                    os.remove(resume_file)
+                pbar.close()
+                print(f"Website downloaded successfully to {output_dir}")
+            else:
+                with open(resume_file, 'w') as f:
+                    f.write(file_name)
+                print("Website download failed. Use the same command to resume download from where it stopped")
+    except subprocess.CalledProcessError as e:
+        print("Website download failed. Error:", e)
+        if os.path.exists(resume_file):
+            os.remove(resume_file)
+
+
+if __name__ == '__main__':
+    url = input("Enter website URL: ")
+    output_dir = '/home/jhembe/localWebSites'
+    max_size = input("Enter maximum size (in MB) to download (optional): ")
+
+    if max_size is not None:
+        max_size = f"{int(max_size) * 1024 * 1024}"
+
+    with tqdm(total=100) as pbar:
+        download_website(url, output_dir, max_size)
